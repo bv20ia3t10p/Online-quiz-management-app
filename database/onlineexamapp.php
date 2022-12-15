18911a19e48ef1cf501ef0b239102c5772f8e5c4
Phạm Trọng Phúc <beaver20310@gmail.com>
2022-12-15 04:08:35 -0800

    Update onlineexamapp.php

--- database/onlineexamapp.php
+++ database/onlineexamapp.php
@@ -201,4 +201,24 @@ if (isset($_GET['getAllScore'])) {
     if (!$id) echo ']';
 }
 
+if (isset($_GET['getLecInfo'])) {
+    $sql = 'select l.name as name, l.id as id, phone, email, c.id as classID, c.name as className, s.description as subject, avg(score) as classAvg, count(id_student_exam) as examNumbers
+    from lecturers l 
+    inner join classes c on l.id = c.ID_lecturer
+    inner join subjects s on c.ID_subject = s.ID
+    inner join student_exams se on c.id = se.id_class 
+    where l.id =' . $_GET['getLecInfo'] . '
+    group by l.name, l.id, phone, email, c.id, c.name, s.description;';
+    $result = mysqli_query($con, $sql);
+    if (!$result) {
+        http_response_code(404);
+        die(mysqli_error($con));
+    }
+    if (!$id) echo '[';
+    for ($i = 0; $i < mysqli_num_rows($result); $i++) {
+        echo ($i > 0 ? ',' : '') . json_encode(mysqli_fetch_object($result));
+    }
+    if (!$id) echo ']';
+}
+
 $con->close();
